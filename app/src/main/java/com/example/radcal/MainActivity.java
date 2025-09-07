d934582140dbad22285b07236420eadc1787aec8
peasti <sirawich.yim@dome.tu.ac.th>
2025-09-07 15:16:07 +0700

    make button can click and use

--- app/src/main/java/com/example/radcal/MainActivity.java
+++ app/src/main/java/com/example/radcal/MainActivity.java
@@ -1,24 +1,50 @@
 package com.example.radcal;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText enterR;
+    Button buttonCalculate;
+    Button buttonClear;
+    TextView areaResult;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        enterR = findViewById(R.id.enterR);
+        buttonCalculate = findViewById(R.id.buttonCalculate);
+        buttonClear = findViewById(R.id.buttonClear);
+        areaResult = findViewById(R.id.areaResult);
+
+        buttonCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String radiusText = enterR.getText().toString();
+                if(!radiusText.isEmpty()) {
+                    double radius = Double.parseDouble(radiusText);
+                    double result = Math.PI * radius * radius;
+                    areaResult.setText(getString(R.string.circle_area) + " = " + result);
+                }else {
+                    areaResult.setText(getString(R.string.enter_r));
+                }
+            }
+        });
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                areaResult.setText(getString(R.string.result));
+            }
         });
     }
 }
